f5d3d39e101dd104c82bb4f8ccc257d8be426bc6
Stefan Kremser <stef.1497@gmail.com>
2018-06-07 20:12:38 +0200

    Made key AND data a generic type (template)

--- examples/StringMap/StringMap.ino
+++ examples/StringMap/StringMap.ino
@@ -7,49 +7,54 @@
 
 #include <SimpleMap.h>
 
-SimpleMap<String>* myMap;
+SimpleMap<String, String>* myMap;
 
 void setup() {
-  Serial.begin(115200);
-  delay(200);
-  Serial.println("STARTED!");
+    Serial.begin(115200);
+    delay(200);
+    Serial.println("STARTED!");
 
-  myMap = new SimpleMap<String>();
-  myMap->put("one","1");
-  myMap->put("two","2");
-  myMap->put("three","3");
+    myMap = new SimpleMap<String, String>([](String a, String b) -> int {
+        if (a > b) return 1;
 
-  Serial.println(myMap->toJson());
-  
-  Serial.println("one: "+myMap->get("one"));
-  Serial.println("two: "+myMap->get("two"));
-  Serial.println("three: "+myMap->get("three"));
-  
-  myMap->put("three","33");
-  Serial.println();
-  
-  Serial.println(myMap->toJson());
-  
-  Serial.println("one: "+myMap->get("one"));
-  Serial.println("two: "+myMap->get("two"));
-  Serial.println("three: "+myMap->get("three"));
+        if (a == b) return 0;
 
-  Serial.println("has three: "+(String)myMap->has("three"));
-  Serial.println("has vier: "+(String)myMap->has("vier"));
+        if (a < b) return -1;
+    });
+    myMap->put("one", "1");
+    myMap->put("two", "2");
+    myMap->put("three", "3");
 
-  myMap->remove("three");
-  
-  Serial.println("has three: "+(String)myMap->has("three"));
-  Serial.println("has vier: "+(String)myMap->has("vier"));
+    Serial.println(myMap->toJson());
 
-  Serial.println(myMap->toJson());
-  myMap->remove("two");
-  Serial.println(myMap->toJson());
+    Serial.println("one: " + myMap->get("one"));
+    Serial.println("two: " + myMap->get("two"));
+    Serial.println("three: " + myMap->get("three"));
+    Serial.println("four: " + myMap->get("four"));
 
-  Serial.println("END!");
+    myMap->put("three", "33");
+    Serial.println();
+
+    Serial.println(myMap->toJson());
+
+    Serial.println("one: " + myMap->get("one"));
+    Serial.println("two: " + myMap->get("two"));
+    Serial.println("three: " + myMap->get("three"));
+    Serial.println("four: " + myMap->get("four"));
+
+    Serial.println("has three: " + (String)myMap->has("three"));
+    Serial.println("has vier: " + (String)myMap->has("vier"));
+
+    myMap->remove("three");
+
+    Serial.println("has three: " + (String)myMap->has("three"));
+    Serial.println("has vier: " + (String)myMap->has("vier"));
+
+    Serial.println(myMap->toJson());
+    myMap->remove("two");
+    Serial.println(myMap->toJson());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    // put your main code here, to run repeatedly:
 }
--- src/SimpleMap.h
+++ src/SimpleMap.h
@@ -1,201 +1,253 @@
 #ifndef SimpleMap_h
 #define SimpleMap_h
 
-template<class T>
+#include <stddef.h>
+#include <functional>
+
+template<class T, class U>
 struct Node {
-  String key;
-  T        data;
-  Node<T>* next;
+    T           key;
+    U           data;
+    Node<T, U>* next;
 };
 
-template<typename T>
+template<typename T, typename U>
 class SimpleMap {
-  public:
-    SimpleMap();
-    ~SimpleMap();
+    public:
+        std::function<int(T a, T b)>compare = NULL;
 
-    int size();
-    void clear();
-    void remove(String key);
-    void put(String key, T obj);
-    T get(String key);
-    bool has(String key);
-    String toJson();
+        SimpleMap(std::function<int(T a, T b)>compare);
+        ~SimpleMap();
 
-  protected:
-    int listSize;
-    Node<T>* listBegin;
-    Node<T>* listEnd;
+        virtual int size();
+        virtual void clear();
+        virtual void remove(T key);
+        virtual void put(T key, U obj);
+        virtual U get(T key);
+        virtual bool has(T key);
+        virtual String toJson();
 
-    Node<T>* getNode(String key);
+    protected:
+        int listSize;
+        Node<T, U>* listBegin;
+        Node<T, U>* listEnd;
+
+        // Helps get() method by saving last position
+        Node<T, U>* lastNodeGot = NULL;
+        int lastIndexGot        = 0;
+        bool isCached           = false;
+
+        virtual Node<T, U>* getNode(T key);
+        virtual Node<T, U>* getNodeIndex(int index);
 };
 
-template<typename T>
-SimpleMap<T>::SimpleMap() {
-  listBegin = NULL;
-  listEnd   = NULL;
-  listSize  = 0;
+template<typename T, typename U>
+SimpleMap<T, U>::SimpleMap(std::function<int(T a, T b)>compare) {
+    SimpleMap<T, U>::compare = compare;
+    listBegin                = NULL;
+    listEnd                  = NULL;
+    listSize                 = 0;
 }
 
 // Clear Nodes and free Memory
-template<typename T>
-SimpleMap<T>::~SimpleMap() {
-  clear();
+template<typename T, typename U>
+SimpleMap<T, U>::~SimpleMap() {
+    clear();
 }
 
-template<typename T>
-void SimpleMap<T>::clear() {
-  Node<T> h = listBegin;
-  Node<T> toDelete;
-  while (h) {
-    toDelete = h;
-    h = h->next;
-    delete toDelete;
-  }
-
-  listBegin = NULL;
-  listEnd   = NULL;
-  listSize  = 0;
+template<typename T, typename U>
+U SimpleMap<T, U>::get(T key) {
+    Node<T, U>* h = getNode(key);
+    return h ? h->data : U();
 }
 
-template<typename T>
-int SimpleMap<T>::size() {
-  return listSize;
-}
+template<typename T, typename U>
+Node<T, U>* SimpleMap<T, U>::getNode(T key) {
+    if (listSize > 0) {
+        if ((compare(key, listBegin->key) < 0) || (compare(key, listEnd->key) > 0)) return NULL;
 
-template<typename T>
-void SimpleMap<T>::put(String key, T obj) {
-  // create new node
-  Node<T>* newNode = new Node<T>();
-  newNode->next = NULL;
-  newNode->data = obj;
-  newNode->key = key;
+        Node<T, U>* h = listBegin;
 
-  // look if already in list
-  Node<T>* h = listBegin;
-  Node<T>* p = NULL;
-  bool found = false;
-  
-  if (listSize > 0) {
-    while (h != NULL && !found) {
-      if (h->key == key) {
-        found = true;
-      } else {
-        p = h;
-        h = h->next;
-      }
-    }
-  }
-
-  // replace old node with new one
-  if (found) {
-    if(h == listBegin) listBegin = newNode;
-    if(h == listEnd) listEnd = newNode;
-    if (p) p->next = newNode;
-    newNode->next = h->next;
-    delete h;
-  } 
-  
-  // create new node
-  else {
-    if (listSize == 0) {
-      // add at start (first node)
-      listBegin = newNode;
-      listEnd = newNode;  
-    } else {
-      if(key >= listEnd->key) {
-        // add at end
-        listEnd->next = newNode;
-        listEnd = newNode;
-      } else if(key <= listBegin->key) {
-        // add at start
-        newNode->next = listBegin;
-        listBegin = newNode;
-      } else {
-        // insertion sort
-        h = listBegin;
-        p = NULL;
-        found = false;
-        while (h != NULL && !found) {
-          if (h->key > key) {
-            found = true;
-          } else {
-            p = h;
-            h = h->next;
-          }
+        while (h) {
+            if (compare(h->key, key) == 0) return h;
+            else h = h->next;
         }
-        newNode->next = h;
-        if (p) p->next = newNode;
-      }
     }
-    
-    listSize++;
-  }
+    return NULL;
 }
 
-template<typename T>
-T SimpleMap<T>::get(String key) {
-  Node<T>* h = getNode(key);
-  return h ? h->data : T();
+template<typename T, typename U>
+Node<T, U>* SimpleMap<T, U>::getNodeIndex(int index) {
+    if ((index < 0) || (index >= listSize)) {
+        return NULL;
+    }
+
+    Node<T, U>* hNode = listBegin;
+    int c             = 0;
+
+    if (isCached && (index >= lastIndexGot)) {
+        c     = lastIndexGot;
+        hNode = lastNodeGot;
+    }
+
+    while (hNode != NULL && c < index) {
+        hNode = hNode->next;
+        c++;
+    }
+
+    if (hNode) {
+        isCached     = true;
+        lastIndexGot = c;
+        lastNodeGot  = hNode;
+    }
+    return hNode;
 }
 
-template<typename T>
-Node<T>* SimpleMap<T>::getNode(String key) {
-  if (listSize > 0) {
-    if(key < listBegin->key || key > listEnd->key) return NULL;
-    Node<T>* h = listBegin;
-    while (h) {
-      if (h->key == key) return h;
-      else h = h->next;
+template<typename T, typename U>
+void SimpleMap<T, U>::clear() {
+    Node<T, U>* h = listBegin;
+    Node<T, U>* toDelete;
+
+    while (h != NULL) {
+        toDelete = h;
+        h        = h->next;
+        delete toDelete;
     }
-  }
-  return NULL;
+
+    listBegin = NULL;
+    listEnd   = NULL;
+    listSize  = 0;
 }
 
-template<typename T>
-void SimpleMap<T>::remove(String key) {
-  if (listSize > 0) {
-    if(key < listBegin->key || key > listEnd->key) return;
-    
-    Node<T>* h = listBegin;
-    Node<T>* p = NULL;
-    bool found = false;
-    while (h != NULL && !found) {
-      if (h->key == key) {
-        found = true;
-      } else {
-        p = h;
-        h = h->next;
-      }
+template<typename T, typename U>
+int SimpleMap<T, U>::size() {
+    return listSize;
+}
+
+template<typename T, typename U>
+void SimpleMap<T, U>::put(T key, U obj) {
+    // create new node
+    Node<T, U>* newNode = new Node<T, U>();
+    newNode->next = NULL;
+    newNode->data = obj;
+    newNode->key  = key;
+
+    // look if already in list
+    Node<T, U>* h = listBegin;
+    Node<T, U>* p = NULL;
+    bool found    = false;
+
+    if (listSize > 0) {
+        while (h != NULL && !found) {
+            if (h->key == key) {
+                found = true;
+            } else {
+                p = h;
+                h = h->next;
+            }
+        }
     }
+
+    // replace old node with new one
     if (found) {
-      if (p) p->next = h->next;
-      else listBegin = h->next;
-      if (listEnd == h) listEnd = p;
-      listSize--;
-      delete h;
+        if (h == listBegin) listBegin = newNode;
+
+        if (h == listEnd) listEnd = newNode;
+
+        if (p) p->next = newNode;
+        newNode->next = h->next;
+        delete h;
     }
-  }
+
+    // create new node
+    else {
+        if (listSize == 0) {
+            // add at start (first node)
+            listBegin = newNode;
+            listEnd   = newNode;
+        } else {
+            if (key >= listEnd->key) {
+                // add at end
+                listEnd->next = newNode;
+                listEnd       = newNode;
+            } else if (key <= listBegin->key) {
+                // add at start
+                newNode->next = listBegin;
+                listBegin     = newNode;
+            } else {
+                // insertion sort
+                h     = listBegin;
+                p     = NULL;
+                found = false;
+
+                while (h != NULL && !found) {
+                    if (compare(h->key, key) > 0) {
+                        found = true;
+                    } else {
+                        p = h;
+                        h = h->next;
+                    }
+                }
+                newNode->next = h;
+
+                if (p) p->next = newNode;
+            }
+        }
+
+        listSize++;
+    }
 }
 
-template<typename T>
-bool SimpleMap<T>::has(String key) {
-  return getNode(key) != NULL;
+template<typename T, typename U>
+void SimpleMap<T, U>::remove(T key) {
+    if (listSize > 0) {
+        if ((compare(key, listBegin->key) < 0) || (compare(key, listEnd->key) > 0)) return;
+
+        Node<T, U>* h = listBegin;
+        Node<T, U>* p = NULL;
+        bool found    = false;
+
+        while (h != NULL && !found) {
+            if (h->key == key) {
+                found = true;
+            } else {
+                p = h;
+                h = h->next;
+            }
+        }
+
+        if (found) {
+            if (p) p->next = h->next;
+            else listBegin = h->next;
+
+            if (listEnd == h) listEnd = p;
+            listSize--;
+            delete h;
+        }
+    }
 }
 
-template<typename T>
-String SimpleMap<T>::toJson() {
-  String json = "{";
-
-  Node<T>* h = listBegin;
-  while (h != NULL) {
-    json += "\"" + h->key + "\":\"" + h->data + "\"";
-    if (h->next) json += ",";
-    h = h->next;
-  }
-
-  json += "}";
-  return json;
+template<typename T, typename U>
+bool SimpleMap<T, U>::has(T key) {
+    return getNode(key) != NULL;
 }
 
-#endif
+template<typename T, typename U>
+String SimpleMap<T, U>::toJson() {
+    String json = "{";
+
+    /*
+        Node<T,U>* h = listBegin;
+
+        while (h != NULL) {
+            json += "\"" + h->key + "\":\"" + h->data + "\"";
+
+            if (h->next) json += ",";
+            h = h->next;
+        }
+     */
+    json += "}";
+    return json;
+}
+
+#endif // ifndef SimpleMap_h
